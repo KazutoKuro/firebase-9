56937d2407591f03d868aec79efda48fff21dc4a
KauztoKuro <ikkikurogane70@gmail.com>
2022-03-22 13:42:58 +0630

    L-3: Setting up Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello from index.js')
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTrzs8_L-G0s34tSjnMUtrUPG8m7q-YB8",
+  authDomain: "fir-9-dojo-65c85.firebaseapp.com",
+  projectId: "fir-9-dojo-65c85",
+  storageBucket: "fir-9-dojo-65c85.appspot.com",
+  messagingSenderId: "261245759225",
+  appId: "1:261245759225:web:a36b824bf61013fe73fc28",
+};
+
+initializeApp(firebaseConfig);
