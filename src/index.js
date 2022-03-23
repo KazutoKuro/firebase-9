cd085716ecb6f4d5dc5cfc6fd4c7c9cb8d2556d8
KauztoKuro <ikkikurogane70@gmail.com>
2022-03-23 13:17:49 +0630

    L-4: Firebase Setup & Fetching Data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCTrzs8_L-G0s34tSjnMUtrUPG8m7q-YB8",
   authDomain: "fir-9-dojo-65c85.firebaseapp.com",
@@ -9,4 +9,25 @@ const firebaseConfig = {
   appId: "1:261245759225:web:a36b824bf61013fe73fc28",
 };
 
+// init firebase app
 initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "books");
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    // console.log(snapshot.docs);
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
